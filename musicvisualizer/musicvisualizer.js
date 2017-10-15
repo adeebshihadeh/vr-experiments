3db3effb9c93702ef52684f398a890c904d617c4
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-10-15 11:59:40 -0700

    cleanup

--- musicvisualizer/musicvisualizer.js
+++ musicvisualizer/musicvisualizer.js
@@ -3,9 +3,6 @@ var scene = document.querySelector("a-scene");
 var eq = new Equalizer();
 eq.play("audio.mp3");
 
-var graph_mode = "bar360";
-
-
 var bar360 = {
   radius: 45,
   init: function(data) {
@@ -33,122 +30,86 @@ var bar360 = {
   }
 };
 
+var bar_graph = {
+  max_width: 20,
+  init: function(data) {
+    for (var i in data) {
+      var el = document.createElement("a-box");
 
-// bar graph
-function bar_graph(data) {
-  var max_width = 25;
+      el.setAttribute("position", {x: i*this.max_width/data.length - ((this.max_width-1)/2), y: 0, z: -5});
+      el.setAttribute("scale", {x: this.max_width/data.length, y: data[i]/120, z: 0.1});
+      el.setAttribute("color", "red");
+      el.className = "bar-graph";
 
-  $(".graph").remove();
-  for (var i in data) {
-    var el = document.createElement("a-box");
-
-    el.setAttribute("position", {x: i*max_width/data.length - ((max_width-1)/2), y: 0, z: -5});
-    el.setAttribute("scale", {x: max_width/data.length, y: data[i]/120, z: 0.1});
-    el.setAttribute("color", "red");
-    el.className = "graph";
-
-    scene.appendChild(el);
+      scene.appendChild(el);
+    }
+  },
+  update: function(data) {
+    for (var i in data) {
+      var el = scene.querySelectorAll(".bar-graph")[i];
+      el.setAttribute("scale", {x: this.max_width/data.length, y: data[i]/120, z: 0.1});
+    }
+  },
+  destroy: function() {
+    $(".bar-graph").remove();
   }
-}
+};
 
-function bar2(data) {
-  var max_width = 25;
 
-  $(".graph").remove();
-  for (var i in data) {
-    var el = document.createElement("a-box");
+var spheres = {
+  separation: 3,
+  scale: 1/325,
+  init: function(data) {
+    for (var i in data) {
+      var el = document.createElement("a-sphere");
 
-    el.setAttribute("position", {x: i - ((max_width-1)/2), y: 0, z: -5});
-    el.setAttribute("scale", {x: max_width/data.length, y: data[i]/120, z: 0.1});
-    el.setAttribute("color", "red");
-    el.className = "graph";
+      el.setAttribute("radius", data[i] * this.scale);
+      el.setAttribute("position", {x: (i - (data.length-1)/2)*this.separation, y: -2.5, z: -7});
+      el.setAttribute("color", "purple");
+      el.className = "sphere";
 
-    scene.appendChild(el);
+      scene.appendChild(el);
+    }
+  },
+  update: function(data) {
+    for (var i in data) {
+      var el = scene.querySelectorAll(".sphere")[i];
+      el.setAttribute("radius", data[i] * this.scale)
+    }
+  },
+  destroy: function() {
+    $(".sphere").remove();
   }
-}
+};
 
 
-function bar360(data) {
-  var radius = 40;
-
-
-  $(".graph").remove();
-  for (var i in data) {
-    var el = document.createElement("a-box");
-
-    var angle = 360/data.length * i;
-
-    el.setAttribute("position", {x: radius*Math.sin(angle*(Math.PI / 180)), y: 0, z: radius*Math.cos(angle*(Math.PI / 180))});
-    el.setAttribute("scale", {x: (360/data.length > 1 ? 1 : 360/data.length), y: data[i]/3 + 5, z: 0.1});
-    el.setAttribute("color", "red");
-    el.className = "graph";
-
-    // polar to 2D
-
-    scene.appendChild(el);
-  }
-}
-
-
-
-
-
-// spheres
-function graph_spheres(data) {
-  $("a-sphere").remove();
-
-  var separation = 3;
-
-  $(".graph").remove();
-  for (var i in data) {
-    var el = document.createElement("a-sphere");
-
-    el.setAttribute("radius", data[i] / 325);
-    el.setAttribute("position", {x: (i - (data.length-1)/2)*separation, y: -2.5, z: -7});
-    el.setAttribute("color", "purple");
-    el.className = "graph";
-
-    scene.appendChild(el);
-  }
-}
-
+var graphs = [bar360, bar_graph, spheres];
 
 function visualize() {
-  switch (graph_mode) {
-    case "spheres":
-      graph_spheres([eq.getSpectrum()[0], eq.getSpectrum()[1], eq.getSpectrum()[2], eq.getSpectrum()[3]]);
-      break;
-    case "bar":
-      bar2(eq.getSpectrum());
-      break;
-    case "bar360":
-      bar360(eq.getSpectrum());
-      break;
-    default:
-      graph_spheres([eq.getSpectrum()[0], eq.getSpectrum()[1], eq.getSpectrum()[2], eq.getSpectrum()[3]]);
-      break;
+  if (scene == null) {
+    scene = document.querySelector("a-scene");
+  } else {
+    graphs[0].update(eq.getSpectrum());
   }
 }
 
 window.onload = function () {
+  graphs[0].init(eq.getSpectrum());
   scene = document.querySelector("a-scene");
 };
 
 window.setInterval(function() {
-  bar360.update(eq.getSpectrum());
-}, 15);
+  visualize()
+}, 30);
 
 
 $(window).keypress(function(e) {
   switch (e.keyCode) {
-    case 98:
-      graph_mode = "bar";
-      break;
-    case 115:
-      graph_mode = "spheres";
-      break;
-    case 51:
-      graph_mode = "bar360";
+    case 32:
+      graphs[0].destroy()
+      graphs.push(graphs[0]);
+      graphs.shift();
+      graphs[0].init(eq.getSpectrum());
       break;
     default:
       console.log("unimplemented key: " + e.keyCode);
